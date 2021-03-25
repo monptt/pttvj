9854b42b7969f3f7dc1cfc31dd47abb6d0c9718c
monptt <ptt_kr@outlook.jp>
2021-03-26 05:16:53 +0900

    debug output test

--- pttvj/main.cpp
+++ pttvj/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.setWindowTitle("pttvj - mainWindow");
+    window.show();
+
+    return app.exec();
 }
--- pttvj/mainwindow.cpp
+++ pttvj/mainwindow.cpp
@@ -1,13 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtCore/QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+// コンストラクタ
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    qDebug() << "MainWindow start";
 }
 
+// デストラクタ
 MainWindow::~MainWindow()
 {
     delete ui;
